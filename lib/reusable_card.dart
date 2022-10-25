30998f4959aa44a4ca99f09a67c0ce27db0395f2
cemtaskin <cemtaskin82@gmail.com>
2022-10-25 20:29:18 +0300

    Add Reusable card, card child prop. Add font awesome lb.

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  Color? color;
-  ReusableCard({this.color});
+  final Color? color;
+  final Widget? cardChild;
+  ReusableCard({@required this.color, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
